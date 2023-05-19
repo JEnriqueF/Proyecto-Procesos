1a082917dee9f38d242f29be376280d0075608de
Froylan <77468347+Froylan-Aarkan@users.noreply.github.com>
2023-05-19 17:38:15 -0600

    SQL con refacciones

---
+++ Script BD Mantenimiento Procesos_ConRefacciones.sql
@@ -0,0 +1,240 @@
+-- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: mantenimientoprocesos
+-- ------------------------------------------------------
+-- Server version	8.0.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cliente`
+--
+
+DROP TABLE IF EXISTS `cliente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cliente` (
+  `idCliente` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `numTelefono` varchar(45) NOT NULL,
+  `correo` varchar(45) NOT NULL,
+  PRIMARY KEY (`idCliente`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cliente`
+--
+
+LOCK TABLES `cliente` WRITE;
+/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `equipocomputo`
+--
+
+DROP TABLE IF EXISTS `equipocomputo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `equipocomputo` (
+  `idEquipoComputo` int NOT NULL AUTO_INCREMENT,
+  `descripcionEquipo` varchar(45) NOT NULL,
+  PRIMARY KEY (`idEquipoComputo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `equipocomputo`
+--
+
+LOCK TABLES `equipocomputo` WRITE;
+/*!40000 ALTER TABLE `equipocomputo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `equipocomputo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `personal`
+--
+
+DROP TABLE IF EXISTS `personal`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `personal` (
+  `idPersonal` int NOT NULL AUTO_INCREMENT,
+  `usuario` varchar(45) NOT NULL,
+  `contrasenia` varchar(45) NOT NULL,
+  `idTipoUsuario` int DEFAULT NULL,
+  PRIMARY KEY (`idPersonal`),
+  KEY `idTipoUsuario` (`idTipoUsuario`),
+  CONSTRAINT `personal_ibfk_1` FOREIGN KEY (`idTipoUsuario`) REFERENCES `tipousuario` (`idTipoUsuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `personal`
+--
+
+LOCK TABLES `personal` WRITE;
+/*!40000 ALTER TABLE `personal` DISABLE KEYS */;
+INSERT INTO `personal` VALUES (1,'administrador','123456',1),(2,'encargado','123456',2);
+/*!40000 ALTER TABLE `personal` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `refaccion`
+--
+
+DROP TABLE IF EXISTS `refaccion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `refaccion` (
+  `idRefaccion` int NOT NULL AUTO_INCREMENT,
+  `nombreRefaccion` varchar(45) NOT NULL,
+  `precioCosto` double NOT NULL,
+  `unidades` int NOT NULL,
+  PRIMARY KEY (`idRefaccion`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `refaccion`
+--
+
+LOCK TABLES `refaccion` WRITE;
+/*!40000 ALTER TABLE `refaccion` DISABLE KEYS */;
+INSERT INTO `refaccion` VALUES (1,'RAM 8GB 3200MHz',800,34),(2,'CPU AMD Ryzen 3 3200G',2000,30),(3,'CPU Intel I5 11400',5399,10),(4,'Motherboard B650 Aorus Pro',8099,20),(5,'Grafica Nvidia Geforce GTX 1660',6099,18),(6,'Grafica Gigabyte AMD Radeon RX 6800',21499,2);
+/*!40000 ALTER TABLE `refaccion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `refaccionesenservicios`
+--
+
+DROP TABLE IF EXISTS `refaccionesenservicios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `refaccionesenservicios` (
+  `idServicio` int DEFAULT NULL,
+  `idRefaccion` int DEFAULT NULL,
+  KEY `idServicio` (`idServicio`),
+  KEY `idRefaccion` (`idRefaccion`),
+  CONSTRAINT `refaccionesenservicios_ibfk_1` FOREIGN KEY (`idServicio`) REFERENCES `servicio` (`idServicio`),
+  CONSTRAINT `refaccionesenservicios_ibfk_2` FOREIGN KEY (`idRefaccion`) REFERENCES `refaccion` (`idRefaccion`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `refaccionesenservicios`
+--
+
+LOCK TABLES `refaccionesenservicios` WRITE;
+/*!40000 ALTER TABLE `refaccionesenservicios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `refaccionesenservicios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `servicio`
+--
+
+DROP TABLE IF EXISTS `servicio`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `servicio` (
+  `idServicio` int NOT NULL AUTO_INCREMENT,
+  `descripcionDiagnostico` varchar(200) NOT NULL,
+  `descripcionMantenimiento` varchar(200) NOT NULL,
+  `cotizacion` double NOT NULL,
+  `estadoServicio` varchar(45) NOT NULL,
+  `montoTotal` double NOT NULL,
+  `idTipoServicio` int DEFAULT NULL,
+  `idCliente` int DEFAULT NULL,
+  `idEquipoComputo` int DEFAULT NULL,
+  PRIMARY KEY (`idServicio`),
+  KEY `idTipoServicio` (`idTipoServicio`),
+  KEY `idCliente` (`idCliente`),
+  KEY `idEquipoComputo` (`idEquipoComputo`),
+  CONSTRAINT `servicio_ibfk_1` FOREIGN KEY (`idTipoServicio`) REFERENCES `tiposervicio` (`idTipoServicio`),
+  CONSTRAINT `servicio_ibfk_2` FOREIGN KEY (`idCliente`) REFERENCES `cliente` (`idCliente`),
+  CONSTRAINT `servicio_ibfk_3` FOREIGN KEY (`idEquipoComputo`) REFERENCES `equipocomputo` (`idEquipoComputo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `servicio`
+--
+
+LOCK TABLES `servicio` WRITE;
+/*!40000 ALTER TABLE `servicio` DISABLE KEYS */;
+/*!40000 ALTER TABLE `servicio` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tiposervicio`
+--
+
+DROP TABLE IF EXISTS `tiposervicio`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tiposervicio` (
+  `idTipoServicio` int NOT NULL AUTO_INCREMENT,
+  `tipoServicio` varchar(45) NOT NULL,
+  `cobroManoObra` double NOT NULL,
+  PRIMARY KEY (`idTipoServicio`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tiposervicio`
+--
+
+LOCK TABLES `tiposervicio` WRITE;
+/*!40000 ALTER TABLE `tiposervicio` DISABLE KEYS */;
+INSERT INTO `tiposervicio` VALUES (1,'Diagnóstico',50),(2,'Correctivo',150),(3,'Preventivo',100);
+/*!40000 ALTER TABLE `tiposervicio` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tipousuario`
+--
+
+DROP TABLE IF EXISTS `tipousuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tipousuario` (
+  `idTipoUsuario` int NOT NULL AUTO_INCREMENT,
+  `tipoUsuario` varchar(45) NOT NULL,
+  PRIMARY KEY (`idTipoUsuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tipousuario`
+--
+
+LOCK TABLES `tipousuario` WRITE;
+/*!40000 ALTER TABLE `tipousuario` DISABLE KEYS */;
+INSERT INTO `tipousuario` VALUES (1,'Administrador'),(2,'EncargadoMantenimiento');
+/*!40000 ALTER TABLE `tipousuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-05-19 17:36:55
